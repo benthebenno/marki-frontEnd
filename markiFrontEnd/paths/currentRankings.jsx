e959c71a5b2f7bb613b4c65104d673ebe3fe6d19
Benjamin Rappoport <ben.rappoport@gmail.com>
2025-09-14 14:21:01 -0400

    got rankings done

--- markiFrontEnd/paths/currentRankings.jsx
+++ markiFrontEnd/paths/currentRankings.jsx
@@ -1,12 +1,54 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { colors } from "../colors";
 import TopBar from "../components/topBar";
 import StockList from "../components/stockList";
 import { ScrollView } from "react-native";
+import { readRemoteFile } from "react-native-csv";
+import { useState, useEffect } from "react";
 
 function CurRank() {
+  const [csvData, setCsvData] = useState([]);
+  const csvFileUrl = "https://benthebenno.github.io/jesse_score_rankings.csv"; // Replace with your actual URL
+
+  readRemoteFile(csvFileUrl, {
+    complete: (results) => {
+      console.log("Parsed CSV data:", results.data);
+      // results.data will contain the parsed CSV data as an array of arrays or objects
+    },
+    error: (error) => {
+      console.error("Error parsing CSV:", error);
+    },
+    // Optional: Add other configuration options like header: true if your CSV has a header row
+  });
+
+  useEffect(() => {
+    readRemoteFile(csvFileUrl, {
+      complete: (results) => {
+        setCsvData(results.data);
+        // console.log("csv parsed");
+        // console.log(csvData);
+      },
+      error: (error) => {
+        console.error("Error parsing CSV:", error);
+      },
+      header: true, // If your CSV has a header row
+    });
+  }, []);
+
+  const renderItem = ({ stock, rank }) => (
+    <View
+      style={{
+        flexDirection: "row",
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: "#ccc",
+      }}
+    >
+      <Text>{item.Stock}</Text>
+    </View>
+  );
   return (
     <View>
       <TopBar></TopBar>
@@ -17,12 +59,23 @@ function CurRank() {
         start={{ x: 0, y: 0 }} // Start point of the gradient (top-left)
         end={{ x: 1, y: 1 }} // End point of the gradient (bottom-right)
       >
-        <View>
-          <Text style={styles.title}>Current Marki Rankings</Text>
+        <View style={{ flex: 1, paddingTop: 50 }}>
+          {csvData.length > 0 ? (
+            <FlatList
+              data={csvData}
+              renderItem={({ item }) => (
+                <View style={{ flexDirection: "row" }}>
+                  <Text>{item.Stock}</Text>
+                  <Text>{item.Rank}</Text>
+                </View>
+              )}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          ) : (
+            <Text>Loading CSV data...</Text>
+          )}
         </View>
       </LinearGradient>
-
-      {/* </ScrollView> */}
     </View>
   );
 }
